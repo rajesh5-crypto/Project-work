2bcf49c92bf58ecc49c3ef8d8d75e4ba196dcc3d
rajesh5-crypto <newar3095@gmail.com>
2025-12-26 11:19:08 +0545

    c programming assignment.
    .....

---
+++ assignment 1 Q3.cpp
@@ -0,0 +1,12 @@
+//WAP to compute area of circle.
+#include<stdio.h>
+int main()
+{
+float radius,area;
+float pi=3.14159;
+printf("enter the radius of circle:");
+scanf("%f",&radius);
+area= pi*(radius*radius);
+printf("the area of a circle is %f",area);
+	return 0;
+}
